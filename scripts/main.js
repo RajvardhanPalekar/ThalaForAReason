f55496f5b5deb6914511df769fab1c2f5d035d63
RajvardhanPalekar <rajvardhan.palekar@gmail.com>
2023-12-22 16:20:44 +0530

    TFAR-03: Adding share button

--- scripts/main.js
+++ scripts/main.js
@@ -88,8 +88,34 @@ function resetContent() {
     // Reset tab on button press
     document.getElementById("content").innerHTML = "<div id=\"content\">" +
                                                         "<h6>Type anything and we will try to find the reason</h6>" +
-                                                        "<h6>( Work in progress )</h6>" +
                                                         "<input type=\"text\" id=\"inputText\" placeholder=\"Type something..\">" +
                                                         "<button onclick=\"calculateReason()\">Submit</button>" +
                                                     "</div>"
 }
+
+
+const sharebtn = 
+    document.querySelector('.sharebtn')
+ 
+// Creating a bool variable for changing
+// the image of share button 
+let bool = 0
+ 
+// Adding an event listener
+sharebtn.addEventListener('click', () => {
+ 
+    // As we clicked the mouse over
+    // the share button the bool value.
+    //  get flipped and then working of
+    // if-else loop get starts
+    bool = !bool
+     
+    if (bool == 0) {
+        sharebtn.innerHTML =
+            '<i class="far fa-share-square"></i>'
+    } else {
+        sharebtn.innerHTML =
+            '<i class="fas fa-times"></i>'
+    }
+})
+
